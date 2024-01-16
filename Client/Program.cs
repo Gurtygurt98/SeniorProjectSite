4ed05dcb9750074f8123ef4a4f1b75f18d9cb9a6
Austin Phillips <austinphillipssps@gmail.com>
2024-01-16 12:02:34 -0500

    Added Document Links

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,11 +2,13 @@ using Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-
+using Syncfusion.Blazor;
+using Syncfusion.Blazor.SfPdfViewer;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
+builder.Services.AddSyncfusionBlazor();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
